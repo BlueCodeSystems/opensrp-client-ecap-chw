452528337e9afcb7224523a7083d7d8d98957149
David ju nyambe <davidnyambe9@gmail.com>
2021-07-06 20:54:01 +0200

    added icons to the navigation menu as well as navigatiooptions to fit poject requirments

--- opensrp-ecap-chw/src/ecap/java/com/bluecodeltd/ecap/chw/model/NavigationModelFlv.java
+++ opensrp-ecap-chw/src/ecap/java/com/bluecodeltd/ecap/chw/model/NavigationModelFlv.java
@@ -20,22 +20,23 @@ public class NavigationModelFlv implements NavigationModel.Flavor {
 
         if (navigationOptions.size() == 0) {
             NavigationOption op1 = new NavigationOption(R.mipmap.sidemenu_families, R.mipmap.sidemenu_families_active, R.string.all_households, Constants.DrawerMenu.ALL_FAMILIES, 0);
-            NavigationOption op2 = new NavigationOption(R.mipmap.sidemenu_children, R.mipmap.sidemenu_children_active, R.string.all_Vcas, Constants.DrawerMenu.CHILD_CLIENTS, 0);
-
+            NavigationOption op2 = new NavigationOption(R.mipmap.sidemenu_children, R.mipmap.sidemenu_children_active, R.string.all_benefeciaries, Constants.DrawerMenu.BENEFICIARIES, 0);
+            NavigationOption op3 = new NavigationOption(R.mipmap.sidemenu_index,R.mipmap.sidemenu_index_active,R.string.all_indexes, Constants.DrawerMenu.INDEXES,0);
+            NavigationOption op4 = new NavigationOption(R.mipmap.sidemenu_case_plan,R.mipmap.sidemenu_case_plan_active,R.string.all_indexes, Constants.DrawerMenu.CASE_PLANS,0);
             //NavigationOption op3 = new NavigationOption(R.mipmap.sidemenu_anc, R.mipmap.sidemenu_anc_active, R.string.menu_anc, Constants.DrawerMenu.ANC, 0);
-            NavigationOption op5 = new NavigationOption(R.mipmap.sidemenu_pnc, R.mipmap.sidemenu_pnc_active, R.string.menu_pnc, Constants.DrawerMenu.PNC, 0);
+            //NavigationOption op5 = new NavigationOption(R.mipmap.sidemenu_pnc, R.mipmap.sidemenu_pnc_active, R.string.menu_pnc, Constants.DrawerMenu.PNC, 0);
             //NavigationOption op6 = new NavigationOption(R.mipmap.sidemenu_fp, R.mipmap.sidemenu_fp_active, R.string.menu_family_planing, Constants.DrawerMenu.FAMILY_PLANNING, 0);
            // NavigationOption op7 = new NavigationOption(R.mipmap.sidemenu_malaria, R.mipmap.sidemenu_malaria_active, R.string.menu_malaria, Constants.DrawerMenu.MALARIA, 0);
-            NavigationOption op8 = new NavigationOption(R.mipmap.sidemenu_referrals, R.mipmap.sidemenu_referrals_active, R.string.menu_referrals, Constants.DrawerMenu.REFERRALS, 0);
+            //NavigationOption op8 = new NavigationOption(R.mipmap.sidemenu_referrals, R.mipmap.sidemenu_referrals_active, R.string.menu_referrals, Constants.DrawerMenu.REFERRALS, 0);
            //NavigationOption op9 = new NavigationOption(R.mipmap.sidemenu_updates, R.mipmap.sidemenu_updates_active, R.string.updates, CoreConstants.DrawerMenu.UPDATES, 0);
 
             if (BuildConfig.USE_UNIFIED_REFERRAL_APPROACH && BuildConfig.BUILD_FOR_BORESHA_AFYA_SOUTH)
                 navigationOptions.add(new NavigationOption(R.mipmap.sidemenu_families, R.mipmap.sidemenu_families_active, R.string.menu_all_clients, CoreConstants.DrawerMenu.ALL_CLIENTS, 0));
 
-            navigationOptions.addAll(Arrays.asList(op1,op2 , op5 ));
+            navigationOptions.addAll(Arrays.asList(op1,op2 ,op3,op4));
 
             if (BuildConfig.USE_UNIFIED_REFERRAL_APPROACH)
-                navigationOptions.add(op8);
+               // navigationOptions.add(op8);
 
             //navigationOptions.add(op9);
         }
