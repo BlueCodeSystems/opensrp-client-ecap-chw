2326a8d53fb8e05fea0e07d2de1d7ec46ab8b734
Elly Kitoto <junkmailstoelly@gmail.com>
2021-07-09 09:11:30 +0300

    Remove unnecessary background services

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/job/ChwJobCreator.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/job/ChwJobCreator.java
@@ -42,26 +42,14 @@ public class ChwJobCreator implements JobCreator {
                 return new PullUniqueIdsServiceJob();
             case ValidateSyncDataServiceJob.TAG:
                 return new ValidateSyncDataServiceJob();
-            case VaccineRecurringServiceJob.TAG:
-                return new VaccineRecurringServiceJob();
             case ImageUploadServiceJob.TAG:
                 return new ImageUploadServiceJob();
-            case P2pServiceJob.TAG:
-                return new P2pServiceJob();
-            case HomeVisitServiceJob.TAG:
-                return new HomeVisitServiceJob();
-            case ChwIndicatorGeneratingJob.TAG:
-                return new ChwIndicatorGeneratingJob();
-            case BasePncCloseJob.TAG:
-                return new BasePncCloseJob();
             case SyncTaskServiceJob.TAG:
                 return (ChwApplication.getApplicationFlavor().hasTasks()) ? new SyncTaskServiceJob(ChwSyncTaskIntentService.class) : null;
             case ScheduleJob.TAG:
                 return new ScheduleJob();
             case SyncLocationsByLevelAndTagsServiceJob.TAG:
                 return new SyncLocationsByLevelAndTagsServiceJob();
-            case StockUsageReportJob.TAG:
-                return new StockUsageReportJob();
             case DocumentConfigurationServiceJob.TAG:
                 return new DocumentConfigurationServiceJob(DocumentConfigurationIntentService.class);
             //TODO uncomment to enable plans
