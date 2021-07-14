82eceeb638729649a8221dadecc23d309dd4d8f4
Chobela <chobelak@gmail.com>
2021-07-14 12:35:10 +0200

    Update Index Register

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/contract/IndexRegisterContract.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/contract/IndexRegisterContract.java
@@ -2,21 +2,43 @@ package com.bluecodeltd.ecap.chw.contract;
 
 import com.bluecodeltd.ecap.chw.domain.ChildIndexEventClient;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.smartregister.view.contract.BaseRegisterContract;
+
+import java.util.List;
 
 public interface IndexRegisterContract {
 
     interface View {
         void toggleDialogVisibility(boolean showDialog);
+
     }
 
     interface Presenter extends BaseRegisterContract.Presenter {
-        void saveForm(String json);
+        void saveForm(String json, boolean isEditMode);
 
         void onRegistrationSaved();
+
+        View getView();
     }
 
     interface Interactor {
-        void saveRegistration(ChildIndexEventClient childIndexEventClient);
+        boolean saveRegistration(final List<ChildIndexEventClient> childEventClientList, final String jsonString, final boolean isEditMode, final InteractorCallBack callBack);
+
+        void onRegistrationSaved(boolean editMode, boolean isSaved, ChildIndexEventClient childEventClient);
+    }
+
+    interface Model {
+        ChildIndexEventClient processRegistration(String jsonString);
+    }
+
+    interface InteractorCallBack {
+
+        void onUniqueIdFetched(Triple<String, String, String> triple, String entityId);
+
+        void onNoUniqueId();
+
+        void onRegistrationSaved(boolean isEditMode, boolean isSaved, List<ChildIndexEventClient> familyEventClientList);
+
     }
 }
