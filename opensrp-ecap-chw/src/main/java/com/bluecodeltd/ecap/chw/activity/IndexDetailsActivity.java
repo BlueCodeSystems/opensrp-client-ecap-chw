089c94385076a14701b9a5243c4cf98520258b26
Chobela <chobelak@gmail.com>
2021-07-29 22:07:35 +0200

    Update VCA Screening Relevance

---
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/activity/IndexDetailsActivity.java
@@ -0,0 +1,22 @@
+package com.bluecodeltd.ecap.chw.activity;
+
+import org.smartregister.view.activity.BaseProfileActivity;
+
+import androidx.viewpager.widget.ViewPager;
+
+public class IndexDetailsActivity extends BaseProfileActivity {
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    protected ViewPager setupViewPager(ViewPager viewPager) {
+        return null;
+    }
+
+    @Override
+    protected void fetchProfileData() {
+
+    }
+}
