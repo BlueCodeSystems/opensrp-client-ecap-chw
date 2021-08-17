85c7b3a75c70dab133369280848183d6e5920496
chobela <chobelak@gmail.com>
2021-08-17 19:45:15 +0200

    Update EC Tables

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/presenter/IndexRegisterFragmentPresenter.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/presenter/IndexRegisterFragmentPresenter.java
@@ -24,10 +24,10 @@ public class IndexRegisterFragmentPresenter implements IndexRegisterFragmentCont
     @Override
     public void initializeQueries(String s) {
 
-        String countSelect = "SELECT COUNT(*) FROM ec_client_index";
-        String mainSelect = "SELECT id as _id, relationalid, relationalid as relational_id, first_name, last_name, physical_address FROM ec_client_index";
+        String countSelect = "SELECT COUNT(*) FROM ec_child";
+        String mainSelect = "SELECT id as _id, relationalid, relationalid as relational_id, first_name FROM ec_child";
 
-        getView().initializeQueryParams("ec_client_index", countSelect, mainSelect);
+        getView().initializeQueryParams("ec_child", countSelect, mainSelect);
         getView().initializeAdapter();
         getView().countExecute();
         getView().filterandSortInInitializeQueries();
