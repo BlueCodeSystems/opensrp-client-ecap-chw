7a5c064a7b94fdef3bac3ac2c17850806425cbc7
David ju nyambe <davidnyambe9@gmail.com>
2021-09-27 16:42:12 +0200

    Added more prepopulated fields and read only on prepopulated fields

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/activity/IndexDetailsActivity.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/activity/IndexDetailsActivity.java
@@ -245,16 +245,31 @@ public class IndexDetailsActivity extends AppCompatActivity {
                     String[] caregiverNAmes = fullCaregiverName.split("\\s+");
                     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
                     String caseworker = prefs.getString("ecap", "");
+                    indexRegisterForm.getJSONObject("step1").getJSONArray("fields").getJSONObject(1).put("value", client.getColumnmaps().get("first_name"));
+                    indexRegisterForm.getJSONObject("step1").getJSONArray("fields").getJSONObject(2).put("value", client.getColumnmaps().get("last_name"));
+                    indexRegisterForm.getJSONObject("step1").getJSONArray("fields").getJSONObject(3).put("value", client.getColumnmaps().get("age"));
+                    indexRegisterForm.getJSONObject("step1").getJSONArray("fields").getJSONObject(4).put("value", client.getColumnmaps().get("gender"));
+
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(4).put("value", "add41");
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(4).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(5).put("value", client.getColumnmaps().get("caregiver_id"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(5).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(6).put("value", client.getColumnmaps().get("gender"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(6).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(7).put("value", caregiverNAmes[0]);
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(7).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(8).put("value", caregiverNAmes[1]);
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(8).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(9).put("value", client.getColumnmaps().get("caregiver_birth_date"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(9).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(25).put("value", client.getColumnmaps().get("caregiver_phone"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(25).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(28).put("value", client.getColumnmaps().get("health_facility"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(28).put("read_only", "true");
                     indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(45).put("value", caseworker);
-                   indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(46).put("value", client.getColumnmaps().get("case_worker_nrc"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(45).put("read_only", "true");
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(46).put("value", client.getColumnmaps().get("case_worker_nrc"));
+                    indexRegisterForm.getJSONObject("step2").getJSONArray("fields").getJSONObject(46).put("read_only", "true");
                     startFormActivity(indexRegisterForm);
 
                 } catch (Exception e) {
