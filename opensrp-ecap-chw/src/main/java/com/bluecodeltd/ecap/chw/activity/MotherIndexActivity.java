d5ecb0c72a45d27c56f8898de9ef0f867eaaeb71
chobela <chobelak@gmail.com>
2022-01-19 17:10:57 +0200

    Modified FSW Form

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/activity/MotherIndexActivity.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/activity/MotherIndexActivity.java
@@ -154,7 +154,7 @@ public class MotherIndexActivity extends BaseRegisterActivity implements MotherI
                     new ChwBottomNavigationListener(this));
             bottomNavigationView.getMenu().removeItem(R.id.action_register);
             bottomNavigationView.getMenu().removeItem(R.id.action_register_index);
-
+            bottomNavigationView.getMenu().removeItem(R.id.action_fsw);
             bottomNavigationView.getMenu().findItem(R.id.action_identifcation).setTitle( "Add Mother");
 
         }
