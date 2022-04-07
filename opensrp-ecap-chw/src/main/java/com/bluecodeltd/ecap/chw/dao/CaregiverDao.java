dcaad679c6750d4bfbe00dba1f0b96ea2e671288
unknown <josephphiri909@gmail.com>
2022-04-07 12:45:15 +0200

    Resolve high priority issues in issue 791

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/dao/CaregiverDao.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/dao/CaregiverDao.java
@@ -1,7 +1,6 @@
 package com.bluecodeltd.ecap.chw.dao;
 
 import com.bluecodeltd.ecap.chw.model.Caregiver;
-import com.bluecodeltd.ecap.chw.model.Household;
 
 import org.smartregister.dao.AbstractDao;
 
@@ -11,7 +10,7 @@ public class CaregiverDao extends AbstractDao {
 
     public static Caregiver getCaregiver (String householdID) {
 
-        String sql = "SELECT caregiver_name, caregiver_nrc, caregiver_sex, caregiver_birth_date, caregiver_phone, caregiver_hiv_status, caregiver_art_number, active_on_treatment, relation FROM ec_household WHERE ec_household.household_id = '" + householdID + "' ";
+        String sql = "SELECT caregiver_name, ward, caregiver_nrc, caregiver_sex, caregiver_birth_date, caregiver_phone, caregiver_hiv_status, caregiver_art_number, active_on_treatment, relation FROM ec_household WHERE ec_household.household_id = '" + householdID + "' ";
 
         List<Caregiver> values = AbstractDao.readData(sql, getCaregiverMap());
 
@@ -25,6 +24,7 @@ public class CaregiverDao extends AbstractDao {
             Caregiver record = new Caregiver();
 
             record.setCaregiver_name(getCursorValue(c, "caregiver_name"));
+            record.setWard(getCursorValue(c, "ward"));
             record.setCaregiver_nrc(getCursorValue(c, "caregiver_nrc"));
             record.setCaregiver_sex(getCursorValue(c, "caregiver_sex"));
             record.setCaregiver_birth_date(getCursorValue(c, "caregiver_birth_date"));
