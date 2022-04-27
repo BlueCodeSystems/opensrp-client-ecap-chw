71c0594bf3e9ebbc7437c948e5d87fdca37182d9
Yosefe27 <chapotokajoseph@gmail.com>
2022-04-27 15:47:39 +0200

    Service Report form - Correction on EC Client fields and Model

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/model/ServiceReportModel.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/model/ServiceReportModel.java
@@ -2,4 +2,310 @@ package com.bluecodeltd.ecap.chw.model;
 
 public class ServiceReportModel {
 
+    private String base_entity_id;
+    private String relational_id;
+    private String unique_id;
+    private String caseworker_name;
+    private String art_date;
+    private String caregiver_name;
+    private String household_id;
+    private String bfirst_name;
+    private String blast_name;
+    private String services;
+    private String calhiv;
+    private String art_number;
+    private String art_facility;
+    private String date_last_vl;
+    private String vl_last_result;
+    private String date_next_vl;
+    private String child_mmd;
+    private String date_next_drugpick;
+    private String vca_service1;
+    private String vca_service2;
+    private String vca_service3;
+    private String other_service_children;
+    private String adolescents_id;
+    private String services_caregiver;
+    private String other_service_caregiver;
+    private String caregiver_id;
+    private String vca_services_household;
+    private String other_service_household;
+    private String household_id_household;
+    private String vl_next_result;
+    private String caseworker_date_signed;
+    private String case_worker_signature;
+    private String primary_caregiver_date_signed;
+    private String primary_caregiver_signature;
+
+    public String getBase_entity_id() {
+        return base_entity_id;
+    }
+
+    public void setBase_entity_id(String base_entity_id) {
+        this.base_entity_id = base_entity_id;
+    }
+
+    public String getRelational_id() {
+        return relational_id;
+    }
+
+    public void setRelational_id(String relational_id) {
+        this.relational_id = relational_id;
+    }
+
+    public String getUnique_id() {
+        return unique_id;
+    }
+
+    public void setUnique_id(String unique_id) {
+        this.unique_id = unique_id;
+    }
+
+    public String getCaseworker_name() {
+        return caseworker_name;
+    }
+
+    public void setCaseworker_name(String caseworker_name) {
+        this.caseworker_name = caseworker_name;
+    }
+
+    public String getArt_date() {
+        return art_date;
+    }
+
+    public void setArt_date(String art_date) {
+        this.art_date = art_date;
+    }
+
+    public String getCaregiver_name() {
+        return caregiver_name;
+    }
+
+    public void setCaregiver_name(String caregiver_name) {
+        this.caregiver_name = caregiver_name;
+    }
+
+    public String getHousehold_id() {
+        return household_id;
+    }
+
+    public void setHousehold_id(String household_id) {
+        this.household_id = household_id;
+    }
+
+    public String getBfirst_name() {
+        return bfirst_name;
+    }
+
+    public void setBfirst_name(String bfirst_name) {
+        this.bfirst_name = bfirst_name;
+    }
+
+    public String getBlast_name() {
+        return blast_name;
+    }
+
+    public void setBlast_name(String blast_name) {
+        this.blast_name = blast_name;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getCalhiv() {
+        return calhiv;
+    }
+
+    public void setCalhiv(String calhiv) {
+        this.calhiv = calhiv;
+    }
+
+    public String getArt_number() {
+        return art_number;
+    }
+
+    public void setArt_number(String art_number) {
+        this.art_number = art_number;
+    }
+
+    public String getArt_facility() {
+        return art_facility;
+    }
+
+    public void setArt_facility(String art_facility) {
+        this.art_facility = art_facility;
+    }
+
+    public String getDate_last_vl() {
+        return date_last_vl;
+    }
+
+    public void setDate_last_vl(String date_last_vl) {
+        this.date_last_vl = date_last_vl;
+    }
+
+    public String getVl_last_result() {
+        return vl_last_result;
+    }
+
+    public void setVl_last_result(String vl_last_result) {
+        this.vl_last_result = vl_last_result;
+    }
+
+    public String getDate_next_vl() {
+        return date_next_vl;
+    }
+
+    public void setDate_next_vl(String date_next_vl) {
+        this.date_next_vl = date_next_vl;
+    }
+
+    public String getChild_mmd() {
+        return child_mmd;
+    }
+
+    public void setChild_mmd(String child_mmd) {
+        this.child_mmd = child_mmd;
+    }
+
+    public String getDate_next_drugpick() {
+        return date_next_drugpick;
+    }
+
+    public void setDate_next_drugpick(String date_next_drugpick) {
+        this.date_next_drugpick = date_next_drugpick;
+    }
+
+    public String getVca_service1() {
+        return vca_service1;
+    }
+
+    public void setVca_service1(String vca_service1) {
+        this.vca_service1 = vca_service1;
+    }
+
+    public String getVca_service2() {
+        return vca_service2;
+    }
+
+    public void setVca_service2(String vca_service2) {
+        this.vca_service2 = vca_service2;
+    }
+
+    public String getVca_service3() {
+        return vca_service3;
+    }
+
+    public void setVca_service3(String vca_service3) {
+        this.vca_service3 = vca_service3;
+    }
+
+    public String getOther_service_children() {
+        return other_service_children;
+    }
+
+    public void setOther_service_children(String other_service_children) {
+        this.other_service_children = other_service_children;
+    }
+
+    public String getAdolescents_id() {
+        return adolescents_id;
+    }
+
+    public void setAdolescents_id(String adolescents_id) {
+        this.adolescents_id = adolescents_id;
+    }
+
+    public String getServices_caregiver() {
+        return services_caregiver;
+    }
+
+    public void setServices_caregiver(String services_caregiver) {
+        this.services_caregiver = services_caregiver;
+    }
+
+    public String getOther_service_caregiver() {
+        return other_service_caregiver;
+    }
+
+    public void setOther_service_caregiver(String other_service_caregiver) {
+        this.other_service_caregiver = other_service_caregiver;
+    }
+
+    public String getCaregiver_id() {
+        return caregiver_id;
+    }
+
+    public void setCaregiver_id(String caregiver_id) {
+        this.caregiver_id = caregiver_id;
+    }
+
+    public String getVca_services_household() {
+        return vca_services_household;
+    }
+
+    public void setVca_services_household(String vca_services_household) {
+        this.vca_services_household = vca_services_household;
+    }
+
+    public String getOther_service_household() {
+        return other_service_household;
+    }
+
+    public void setOther_service_household(String other_service_household) {
+        this.other_service_household = other_service_household;
+    }
+
+    public String getHousehold_id_household() {
+        return household_id_household;
+    }
+
+    public void setHousehold_id_household(String household_id_household) {
+        this.household_id_household = household_id_household;
+    }
+
+    public String getVl_next_result() {
+        return vl_next_result;
+    }
+
+    public void setVl_next_result(String vl_next_result) {
+        this.vl_next_result = vl_next_result;
+    }
+
+    public String getCaseworker_date_signed() {
+        return caseworker_date_signed;
+    }
+
+    public void setCaseworker_date_signed(String caseworker_date_signed) {
+        this.caseworker_date_signed = caseworker_date_signed;
+    }
+
+    public String getCase_worker_signature() {
+        return case_worker_signature;
+    }
+
+    public void setCase_worker_signature(String case_worker_signature) {
+        this.case_worker_signature = case_worker_signature;
+    }
+
+    public String getPrimary_caregiver_date_signed() {
+        return primary_caregiver_date_signed;
+    }
+
+    public void setPrimary_caregiver_date_signed(String primary_caregiver_date_signed) {
+        this.primary_caregiver_date_signed = primary_caregiver_date_signed;
+    }
+
+    public String getPrimary_caregiver_signature() {
+        return primary_caregiver_signature;
+    }
+
+    public void setPrimary_caregiver_signature(String primary_caregiver_signature) {
+        this.primary_caregiver_signature = primary_caregiver_signature;
+    }
 }
