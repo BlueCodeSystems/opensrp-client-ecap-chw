72f8183a8c8eb82719ea43cc569ad19b91d9ebe0
chobela <chobelak@gmail.com>
2022-06-06 11:15:18 +0200

    Add Wrong Entry option to De Registration Form

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/fragment/HouseholdVisitsFragment.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/fragment/HouseholdVisitsFragment.java
@@ -4,24 +4,71 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bluecodeltd.ecap.chw.R;
+import com.bluecodeltd.ecap.chw.activity.HouseholdDetails;
 import com.bluecodeltd.ecap.chw.activity.IndexDetailsActivity;
+import com.bluecodeltd.ecap.chw.adapter.CaregiverVisitAdapter;
+import com.bluecodeltd.ecap.chw.adapter.VisitAdapter;
+import com.bluecodeltd.ecap.chw.dao.CaregiverVisitationDao;
+import com.bluecodeltd.ecap.chw.dao.VcaVisitationDao;
+import com.bluecodeltd.ecap.chw.model.CaregiverVisitationModel;
+import com.bluecodeltd.ecap.chw.model.Household;
+import com.bluecodeltd.ecap.chw.model.VcaVisitationModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HouseholdVisitsFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    RecyclerView.Adapter recyclerViewadapter;
+    private ArrayList<CaregiverVisitationModel> visitList = new ArrayList<>();
+    private LinearLayout linearLayout;
+    View vieww;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_housevisits, container, false);
+        View vieww = inflater.inflate(R.layout.fragment_housevisits, container, false);
+
+        vieww = inflater.inflate(R.layout.fragment_childvisits, container, false);
+
+        HashMap<String, Household> mymap = ( (HouseholdDetails) requireActivity()).getData();
+        Household house = mymap.get("house");
+        String houseId = house.getHousehold_id();
+
+        recyclerView = vieww.findViewById(R.id.visitrecyclerView);
+        linearLayout = vieww.findViewById(R.id.visit_container);
+
+        visitList.clear();
+
+        visitList.addAll(CaregiverVisitationDao.getVisitsByID(houseId));
+
+        RecyclerView.LayoutManager eLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(eLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewadapter = new CaregiverVisitAdapter(visitList, getContext());
+        recyclerView.setAdapter(recyclerViewadapter);
+        recyclerViewadapter.notifyDataSetChanged();
+
+        if (recyclerViewadapter.getItemCount() > 0){
+
+            linearLayout.setVisibility(View.GONE);
+        }
 
 
-        return view;
+        return vieww;
+
 
     }
 
