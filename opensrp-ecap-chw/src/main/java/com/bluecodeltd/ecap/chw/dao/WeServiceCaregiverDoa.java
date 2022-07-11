3fa0a17e475dab9f9626ccee75b5f6c8dc09aec7
Yosefe27 <chapotokajoseph@gmail.com>
2022-07-11 15:24:42 +0200

    WE form Models And DAO complete - Caregiver and VCA

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/dao/WeServiceCaregiverDoa.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/dao/WeServiceCaregiverDoa.java
@@ -30,8 +30,24 @@ public class WeServiceCaregiverDoa extends AbstractDao {
             record.setHousehold_id(getCursorValue(c, "household_id"));
             record.setCaregiver_id(getCursorValue(c, "caregiver_id"));
             record.setDate_joined_we(getCursorValue(c, "date_joined_we"));
-
-
+            record.setWe_group_no(getCursorValue(c, "we_group_no"));
+            record.setReporting_month(getCursorValue(c, "reporting_month"));
+            record.setWe_member_in_period(getCursorValue(c, "we_member_in_period"));
+            record.setWe_loan_in_period(getCursorValue(c, "we_loan_in_period"));
+            record.setWe_loan_value1(getCursorValue(c, "we_loan_value1"));
+            record.setWe_loan_value2(getCursorValue(c, "we_loan_value2"));
+            record.setWe_loans1_on_school(getCursorValue(c, "we_loans1_on_school"));
+            record.setWe_loans2_on_school(getCursorValue(c, "we_loans2_on_school"));
+            record.setWe_loans1_on_medical(getCursorValue(c, "we_loans1_on_medical"));
+            record.setWe_loans2_on_medical(getCursorValue(c, "we_loans2_on_medical"));
+            record.setWe_loan1_on_iga(getCursorValue(c, "we_loan1_on_iga"));
+            record.setWe_loan2_on_iga(getCursorValue(c, "we_loan2_on_iga"));
+            record.setWe_loan1_iga_type(getCursorValue(c, "we_loan1_iga_type"));
+            record.setWe_loan2_iga_type(getCursorValue(c, "we_loan2_iga_type"));
+            record.setWe_loan1_use(getCursorValue(c, "we_loan1_use"));
+            record.setWe_loan2_use(getCursorValue(c, "we_loan2_use"));
+            record.setSold_items(getCursorValue(c, "sold_items"));
+            record.setOther_sold_item(getCursorValue(c, "other_sold_item"));
             return record;
         };
     }
