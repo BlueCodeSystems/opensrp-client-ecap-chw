ebf77b2a00d779e55159d4b250798629a3b302fb
joseph <josephphiri909@gmail.com>
2023-06-01 09:23:54 +0200

    Fix editing of the we-service form

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/dao/WeServiceCaregiverDoa.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/dao/WeServiceCaregiverDoa.java
@@ -32,18 +32,24 @@ public class WeServiceCaregiverDoa extends AbstractDao {
             record.setWe_group_name(getCursorValue(c, "we_group_name"));
             record.setDate_joined_we(getCursorValue(c, "date_joined_we"));
             record.setReporting_month(getCursorValue(c, "reporting_month"));
-//            record.setWe_loans1_on_school(getCursorValue(c, "we_loans1_on_school"));
-//            record.setWe_loans2_on_school(getCursorValue(c, "we_loans2_on_school"));
-//            record.setWe_loans1_on_medical(getCursorValue(c, "we_loans1_on_medical"));
-//            record.setWe_loans2_on_medical(getCursorValue(c, "we_loans2_on_medical"));
-//            record.setWe_loan1_on_iga(getCursorValue(c, "we_loan1_on_iga"));
-//            record.setWe_loan2_on_iga(getCursorValue(c, "we_loan2_on_iga"));
-//            record.setWe_loan1_iga_type(getCursorValue(c, "we_loan1_iga_type"));
-//            record.setWe_loan2_iga_type(getCursorValue(c, "we_loan2_iga_type"));
-//            record.setWe_loan1_use(getCursorValue(c, "we_loan1_use"));
-//            record.setWe_loan2_use(getCursorValue(c, "we_loan2_use"));
-//            record.setSold_items(getCursorValue(c, "sold_items"));
-//            record.setOther_sold_item(getCursorValue(c, "other_sold_item"));
+            record.setWe_member_in_period(getCursorValue(c, "we_member_in_period"));
+            record.setWe_loan_in_period(getCursorValue(c, "we_loan_in_period"));
+            record.setWe_loans_taken_in_period(getCursorValue(c, "we_loans_taken_in_period"));
+            record.setWe_loan_value1(getCursorValue(c, "we_loan_value1"));
+
+
+            record.setWe_loans1_on_school(getCursorValue(c, "we_loans1_on_school"));
+            record.setWe_loans2_on_school(getCursorValue(c, "we_loans2_on_school"));
+            record.setWe_loans1_on_medical(getCursorValue(c, "we_loans1_on_medical"));
+            record.setWe_loans2_on_medical(getCursorValue(c, "we_loans2_on_medical"));
+            record.setWe_loan1_on_iga(getCursorValue(c, "we_loan1_on_iga"));
+            record.setWe_loan2_on_iga(getCursorValue(c, "we_loan2_on_iga"));
+            record.setWe_loan1_iga_type(getCursorValue(c, "we_loan1_iga_type"));
+            record.setWe_loan2_iga_type(getCursorValue(c, "we_loan2_iga_type"));
+            record.setWe_loan1_use(getCursorValue(c, "we_loan1_use"));
+            record.setWe_loan2_use(getCursorValue(c, "we_loan2_use"));
+            record.setSold_items(getCursorValue(c, "sold_items"));
+            record.setOther_sold_item(getCursorValue(c, "other_sold_item"));
             return record;
         };
     }
