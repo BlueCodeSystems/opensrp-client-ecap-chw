983e2e8bd60bd0c51bd126a19ac99b3738574223
joseph <josephphiri909@gmail.com>
2023-08-03 16:15:10 +0200

    HTS client

--- opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/model/HivTestingServiceModel.java
+++ opensrp-ecap-chw/src/main/java/com/bluecodeltd/ecap/chw/model/HivTestingServiceModel.java
@@ -3,37 +3,22 @@ package com.bluecodeltd.ecap.chw.model;
 public class HIVTestingServiceModel {
     private String base_entity_id;
     private String relational_id;
+    private String caseworker_name;
+    private String phone;
     private String implementing_partner;
     private String health_facility;
-    private String province;
     private String district;
+    private String province;
     private String client_number;
     private String testing_modality;
     private String first_name;
     private String middle_name;
     private String last_name;
-    private String on_art;
     private String art_number;
     private String gender;
     private String birthdate;
-    private String case_snn_entry_point;
-    private String case_snn_house_number;
-    private String individual_tested;
-    private String ecap_id;
-    private String sub_population;
-    private String individual_index_SNS_birthdate;
-    private String index_sns_relationship;
-    private String current_address_landmarks;
-    private String hiv_status;
-    private String date_tested;
-    private String hiv_result;
-    private String test_done_hf;
-    private String hiv_recent_test;
-    private String art_date;
-    private String art_date_initiated;
-    private String comment;
-    private String caseworker_name;
-    private String checked_by;
+    private String entry_point;
+    private String delete_status;
 
     public String getBase_entity_id() {
         return base_entity_id;
@@ -49,6 +34,22 @@ public class HIVTestingServiceModel {
 
     public void setRelational_id(String relational_id) {
         this.relational_id = relational_id;
+    }
+
+    public String getCaseworker_name() {
+        return caseworker_name;
+    }
+
+    public void setCaseworker_name(String caseworker_name) {
+        this.caseworker_name = caseworker_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getImplementing_partner() {
@@ -67,20 +68,20 @@ public class HIVTestingServiceModel {
         this.health_facility = health_facility;
     }
 
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
     public String getDistrict() {
         return district;
     }
 
     public void setDistrict(String district) {
         this.district = district;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
     }
 
     public String getClient_number() {
@@ -123,14 +124,6 @@ public class HIVTestingServiceModel {
         this.last_name = last_name;
     }
 
-    public String getOn_art() {
-        return on_art;
-    }
-
-    public void setOn_art(String on_art) {
-        this.on_art = on_art;
-    }
-
     public String getArt_number() {
         return art_number;
     }
@@ -155,147 +148,19 @@ public class HIVTestingServiceModel {
         this.birthdate = birthdate;
     }
 
-    public String getCase_snn_entry_point() {
-        return case_snn_entry_point;
+    public String getEntry_point() {
+        return entry_point;
     }
 
-    public void setCase_snn_entry_point(String case_snn_entry_point) {
-        this.case_snn_entry_point = case_snn_entry_point;
+    public void setEntry_point(String entry_point) {
+        this.entry_point = entry_point;
     }
 
-    public String getCase_snn_house_number() {
-        return case_snn_house_number;
+    public String getDelete_status() {
+        return delete_status;
     }
 
-    public void setCase_snn_house_number(String case_snn_house_number) {
-        this.case_snn_house_number = case_snn_house_number;
-    }
-
-    public String getIndividual_tested() {
-        return individual_tested;
-    }
-
-    public void setIndividual_tested(String individual_tested) {
-        this.individual_tested = individual_tested;
-    }
-
-    public String getEcap_id() {
-        return ecap_id;
-    }
-
-    public void setEcap_id(String ecap_id) {
-        this.ecap_id = ecap_id;
-    }
-
-    public String getSub_population() {
-        return sub_population;
-    }
-
-    public void setSub_population(String sub_population) {
-        this.sub_population = sub_population;
-    }
-
-    public String getIndividual_index_SNS_birthdate() {
-        return individual_index_SNS_birthdate;
-    }
-
-    public void setIndividual_index_SNS_birthdate(String individual_index_SNS_birthdate) {
-        this.individual_index_SNS_birthdate = individual_index_SNS_birthdate;
-    }
-
-    public String getIndex_sns_relationship() {
-        return index_sns_relationship;
-    }
-
-    public void setIndex_sns_relationship(String index_sns_relationship) {
-        this.index_sns_relationship = index_sns_relationship;
-    }
-
-    public String getCurrent_address_landmarks() {
-        return current_address_landmarks;
-    }
-
-    public void setCurrent_address_landmarks(String current_address_landmarks) {
-        this.current_address_landmarks = current_address_landmarks;
-    }
-
-    public String getHiv_status() {
-        return hiv_status;
-    }
-
-    public void setHiv_status(String hiv_status) {
-        this.hiv_status = hiv_status;
-    }
-
-    public String getDate_tested() {
-        return date_tested;
-    }
-
-    public void setDate_tested(String date_tested) {
-        this.date_tested = date_tested;
-    }
-
-    public String getHiv_result() {
-        return hiv_result;
-    }
-
-    public void setHiv_result(String hiv_result) {
-        this.hiv_result = hiv_result;
-    }
-
-    public String getTest_done_hf() {
-        return test_done_hf;
-    }
-
-    public void setTest_done_hf(String test_done_hf) {
-        this.test_done_hf = test_done_hf;
-    }
-
-    public String getHiv_recent_test() {
-        return hiv_recent_test;
-    }
-
-    public void setHiv_recent_test(String hiv_recent_test) {
-        this.hiv_recent_test = hiv_recent_test;
-    }
-
-    public String getArt_date() {
-        return art_date;
-    }
-
-    public void setArt_date(String art_date) {
-        this.art_date = art_date;
-    }
-
-    public String getArt_date_initiated() {
-        return art_date_initiated;
-    }
-
-    public void setArt_date_initiated(String art_date_initiated) {
-        this.art_date_initiated = art_date_initiated;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public String getCaseworker_name() {
-        return caseworker_name;
-    }
-
-    public void setCaseworker_name(String caseworker_name) {
-        this.caseworker_name = caseworker_name;
-    }
-
-    public String getChecked_by() {
-        return checked_by;
-    }
-
-    public void setChecked_by(String checked_by) {
-        this.checked_by = checked_by;
+    public void setDelete_status(String delete_status) {
+        this.delete_status = delete_status;
     }
 }
